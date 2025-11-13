c6f237d63d5a1e345a6305fbefe41348ea08369b
gprasad0 <guru96392@gmail.com>
2025-11-13 10:05:56 +0530

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+
 load_dotenv(override=True)
-geminiApiKey = os.getenv('GOOGLE_GEMINI_API_KEY')
+geminiApiKey = os.getenv("GOOGLE_GEMINI_API_KEY")
 print(geminiApiKey)
+
+
+def handlegenAI():
+    print("hi")
+    messages = [{"role": "user", "content": "Something here"}]
+    client = OpenAI(
+        api_key=geminiApiKey,
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    )
+    response = client.chat.completions.create(
+        model="gemini-2.5-flash",
+        messages=[
+            {"role": "user", "content": "Pick a business area that might be worth exploring for an agentic ai oppurtunity"},
+        ],
+    )
+    answer =response.choices[0].message
+    print(answer)
+
+
+
+handlegenAI()
