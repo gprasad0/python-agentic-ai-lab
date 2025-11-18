1d6171e652d453fb7c153015d471a48bcd524c50
gprasad0 <guru96392@gmail.com>
2025-11-18 10:23:15 +0530

    adding black formatter

--- agenticAiLab/callingGenAiApis/main.py
+++ agenticAiLab/callingGenAiApis/main.py
@@ -1,21 +1,33 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import asyncio
+import httpx
+
 
 def handlegenAI():
     load_dotenv(override=True)
     geminiApiKey = os.getenv("GOOGLE_GEMINI_API_KEY")
-    messages =  [{"role": "user", "content": "Pick a business area that might be worth exploring for an agentic ai oppurtunity"}]
-    client = OpenAI(
-        api_key=geminiApiKey,
-        base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-    )
-    response = client.chat.completions.create(
-        model="gemini-2.5-flash",
-        messages=messages
-    )
-    answer =response.choices[0].message
-    print(answer)
+    options = ["business", "technology", "healthcare", "finance", "education"]
+    statments = []
 
-if __name__ == "__main__": 
-  handlegenAI()
+    # messages =  [{"role": "user", "content": "Pick a business area that might be worth exploring for an agentic ai oppurtunity"}]
+    # client = OpenAI(
+    #     api_key=geminiApiKey,
+    #     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    # )
+    # response = client.chat.completions.create(
+    #     model="gemini-2.5-flash",
+    #     messages=messages
+    # )
+    # answer =response.choices[0].message
+    # print(answer)
+    for option in options:
+        businessStatement = f"Pick a business area that might be worth exploring for an agentic ai oppurtunity in this field:{option}"
+        statments.append(businessStatement)
+
+    print(statments)
+
+
+if __name__ == "__main__":
+    handlegenAI()
