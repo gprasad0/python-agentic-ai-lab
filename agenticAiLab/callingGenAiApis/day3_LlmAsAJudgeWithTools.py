14b354649a906cc7a2c27ac037feb332c14630ca
gprasad0 <guru96392@gmail.com>
2026-01-08 10:24:40 +0530

    making tool calls by making a different logic

--- agenticAiLab/callingGenAiApis/day3_LlmAsAJudgeWithTools.py
+++ agenticAiLab/callingGenAiApis/day3_LlmAsAJudgeWithTools.py
@@ -1,54 +1,19 @@
 from dotenv import load_dotenv
-from openai import OpenAI
 import os
-import requests
-from pypdf import PdfReader
 import gradio as gr
+from pypdf import PdfReader
 
 load_dotenv(override=True)
-geminiApiKey = os.getenv("GOOGLE_GEMINI_API_KEY")
-pushoverUser = os.getenv("PUSHOVER_USER")
-pushoverToken = os.getenv("PUSHOVER_TOKEN")
-pushoverUrl = "https://api.pushover.net/1/messages.json"
 
 
-def push(message):
-    print(f"Sending push notification: {message}")
-    payload = {"user": pushoverUser, "token": pushoverToken, "message": message}
-    requests.post(pushoverUrl, data=payload)
-
-
-def record_user_details(email, name="Name not provided", notes="not provided"):
-    push(f"Recording {name} with email {email} and notes {notes}")
-    return {"recorded": "ok"}
-
-
-def record_unknown_question(question):
-    push(f"Recording {question}")
-    return {"recorded": "ok"}
-
-
-class Me:
-    def __init__(self):
-        self.name = "Guru"
-        self.email = "guru@example.com"
-        self.client = OpenAI(
-            api_key=geminiApiKey,
-            base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-        )
-
-    def callGradle(self, message, history):
-        print("rsponse111")
-        messages = [{"role": "user", "content": message}]
-        response = self.client.chat.completions.create(
-            model="gemini-2.5-flash", messages=messages
-        )
-        print("rsponse", response.choices[0].message.content)
-        return response.choices[0].message.content
+def readPdf():
+    reader = PdfReader(f"{os.getenv('LINKEDIN_PDF')}/linkedin.pdf")
+    pdfText = ""
+    for page in reader.pages:
+        pdfText = page.extract_text() + "\n"
+    return pdfText
 
 
 # if __name__ == "__main__":
-#     me = Me()
-
-
-# record_user_details -> record unknown question ->
+#     text = readPdf()
+#     print(text)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # from agenticAiLab.callingGenAiApis.day2_LlmAsAJudgeWithoutTools import chat
 import gradio as gr
-from agenticAiLab.callingGenAiApis.day3_LlmAsAJudgeWithTools import Me
+from agenticAiLab.callingGenAiApis.day3_LlmAsAJudgeWithTools import readPdf
 
-me = Me()
-# push("This is a test push notification from the main.py file.")
-gr.ChatInterface(
-    fn=me.callGradle,
-).launch()
+# from agenticAiLab.callingGenAiApis.day3_LlmAsAJudgeWithTools import Me
+
+# me = Me()
+# # push("This is a test push notification from the main.py file.")
+# gr.ChatInterface(
+#     fn=me.callGradle,
+# ).launch()
+
+if __name__ == "__main__":
+    text = readPdf()
+    print(text)
