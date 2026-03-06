69981e280650d8bd8c832c01584c11cf0b805c0e
gprasad0 <guru96392@gmail.com>
2026-03-06 08:46:20 +0530

    basic tooling for open ai agents sdk

--- agenticAiLab/openAiSdkImplementation/implementation3tooling.py
+++ agenticAiLab/openAiSdkImplementation/implementation3tooling.py
@@ -7,26 +7,21 @@ import requests
 load_dotenv(override=True)
 geminiApiKey = os.getenv("GOOGLE_GEMINI_API_KEY")
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_GEMINI_API_KEY")
-
+geminiLlmModel = os.getenv("gemini_llm_model")
 agent1 = Agent(
     name="joker1",
-    instructions="You are a joker. Tell a joke in one sentence.",
-    model="litellm/gemini/gemini-2.5-flash",
+    instructions="You are a joker. Tell a joke in one sentence about engineers",
+    model=geminiLlmModel,
 )
 agent2 = Agent(
     name="joker2",
-    instructions="You are a joker. Tell a joke in one sentence.",
-    model="litellm/gemini/gemini-2.5-flash",
-)
-evaluationAgent = Agent(
-    name="jokeEvaluator",
-    instructions="You are a joke evaluator. Evaluate the jokes from the payload and tell which one is better and why.",
-    model="litellm/gemini/gemini-2.5-flash",
+    instructions="You are a joker. Tell a joke in one sentence about doctors",
+    model=geminiLlmModel,
 )
 
 
 @function_tool
-def sendNotificationToLaptop(jokeEvaluationResult: str):
+async def sendNotificationToLaptop(jokeEvaluationResult: str):
     """
     Send a notification to laptop with the joke evaluation result.
     """
@@ -38,16 +33,30 @@ def sendNotificationToLaptop(jokeEvaluationResult: str):
     return {"status": "successs"}
 
 
-print("tool1==> ", agent1)
 tool1 = agent1.as_tool(
-    tool_name="jokeTool", tool_description="A tool to get a joke from joker1"
+    tool_name="engineerJokeTool",
+    tool_description="A tool to tell a joke about engineers",
 )
 tool2 = agent2.as_tool(
-    tool_name="jokeTool", tool_description="A tool to get a joke from joker2"
+    tool_name="doctorJokeTool", tool_description="A tool to tell a joke about doctors"
 )
 
 total_tools = [tool1, tool2, sendNotificationToLaptop]
 
+evaluationAgent = Agent(
+    name="jokeEvaluator",
+    instructions="You are a joke evaluator. Evaluate jokes",
+    tools=total_tools,
+    model=geminiLlmModel,
+)
+
 
 async def evaluateAndSendJoke():
-    result = await Runner.run()
+    result = await Runner.run(
+        evaluationAgent,
+        "Evaluate the jokes from the tools given. Use sendNotificationToLaptop tool to send a notification to the laptop with the joke you seem to like better and why",
+    )
+    return result
+
+
+evaluationResult = asyncio.run(evaluateAndSendJoke())
