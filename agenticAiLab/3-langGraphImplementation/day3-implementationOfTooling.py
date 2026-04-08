1bb1c684458510c42176e6815f7d974b5e3cbd15
gprasad0 <guru96392@gmail.com>
2026-04-08 10:16:35 +0530

    adding serper search and push notification

--- agenticAiLab/3-langGraphImplementation/day3-implementationOfTooling.py
+++ agenticAiLab/3-langGraphImplementation/day3-implementationOfTooling.py
@@ -7,6 +7,10 @@ from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
 from langchain.agents import Tool
 import os
+import requests
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import gradio as gr
 
 load_dotenv(override=True)
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_GEMINI_API_KEY")
@@ -22,10 +26,57 @@ tool_search = Tool(
 tool_search.invoke("What is the capital of France?")
 
 
+def pushNotification(message: str):
+    """Push a notification to the user using ntfy"""
+    response = requests.post(
+        os.getenv("NTFY_URL"),
+        data=message,
+        headers={"Title": "Task Success"},
+    )
+    return {"status": response.status_code, "message": "Notification sent successfully"}
+
+
+send_notification_tool = Tool(
+    name="send_notification",
+    func=pushNotification,
+    description="use this tool to send a notification to the user. The input to this tool should be the message you want to send.",
+)
+
+tools = [tool_search, send_notification_tool]
+
+
 # create state
 class State(BaseModel):
     messages: Annotated[list, add_messages]
 
 
+gemini_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm_with_tools = gemini_llm.bind_tools(tools)
 # start the graph builder
 graph_builder = StateGraph(State)
+
+
+# create Node
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+# add nodes
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=tools))
+
+graph_builder.add_conditional_edges("chatbot", tools_condition, "tools")
+
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+graph = graph_builder.compile()
+
+
+def chat(user_input: str, history):
+    result = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+    return result["messages"][-1].content
+
+
+gr.ChatInterface(chat, type="messages").launch()
