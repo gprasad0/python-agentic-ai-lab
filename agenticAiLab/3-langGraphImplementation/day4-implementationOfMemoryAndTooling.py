55f3e23787e63ec85c218eb5227436276bfdb073
gprasad0 <guru96392@gmail.com>
2026-04-16 10:37:46 +0530

    adding simple langraph structure

--- agenticAiLab/3-langGraphImplementation/day4-implementationOfMemoryAndTooling.py
+++ agenticAiLab/3-langGraphImplementation/day4-implementationOfMemoryAndTooling.py
@@ -3,9 +3,30 @@ from pydantic import BaseModel
 from typing import Annotated, TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain.tools import tool
 import gradio as gr
+import os
+import requests
 
 load_dotenv(override=True)
+
+
+# create tools
+@tool
+def pushNotification(input_message: str):
+    """Push a notification to the user using ntfy"""
+    response = requests.post(
+        os.getenv("NTFY_URL"),
+        data=input_message,
+        headers={"Title": "Task Success"},
+    )
+    return {"status": response.status_code, "message": "Notification sent successfully"}
+
+
+@tool
+def searchWrapper():
+    """Searches the web for upto date information"""
 
 
 # create state
@@ -18,7 +39,7 @@ graph_builder = StateGraph(State)
 
 
 # create a node
-def first_node():
+def first_node(messages):
     return "hello"
 
 
