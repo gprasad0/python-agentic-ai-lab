da448708d8c1e4d4bdcc4983a8105abd83db5f9c
gprasad0 <guru96392@gmail.com>
2026-04-18 19:18:04 +0530

    added langsmith

--- agenticAiLab/3-langGraphImplementation/day4-implementationOfMemoryAndTooling.py
+++ agenticAiLab/3-langGraphImplementation/day4-implementationOfMemoryAndTooling.py
@@ -5,28 +5,42 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain.tools import tool
+from langchain_community.utilities import GoogleSerperAPIWrapper
 import gradio as gr
 import os
 import requests
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv(override=True)
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_GEMINI_API_KEY")
+os.getenv("SERPER_API_KEY")
+
+search = GoogleSerperAPIWrapper()
 
 
 # create tools
 @tool
-def pushNotification(input_message: str):
+def pushNotification(input_message: str) -> str:
     """Push a notification to the user using ntfy"""
     response = requests.post(
         os.getenv("NTFY_URL"),
         data=input_message,
         headers={"Title": "Task Success"},
     )
-    return {"status": response.status_code, "message": "Notification sent successfully"}
+    return f"Notification sent: {response.status_code}"
 
 
 @tool
-def searchWrapper():
+def searchWrapper(query: str) -> str:
     """Searches the web for upto date information"""
+    searchedquery = search.run(query)
+    return searchedquery
+
+
+tools = [pushNotification, searchWrapper]
+gemini_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+llm_with_tools = gemini_llm.bind_tools(tools)
 
 
 # create state
@@ -39,23 +53,25 @@ graph_builder = StateGraph(State)
 
 
 # create a node
-def first_node(messages):
-    return "hello"
+def first_node(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
 # add a node
 graph_builder.add_node("first_node", first_node)
+graph_builder.add_node("tools", ToolNode(tools=tools))
 # create edges
 graph_builder.add_edge(START, "first_node")
-graph_builder.add_edge("first_node", END)
+graph_builder.add_conditional_edges("first_node", tools_condition)
+graph_builder.add_edge("tools", "first_node")
 
 # compile the graph
 graph = graph_builder.compile()
 
 
-def chat():
-    # invoke graph
-    graph.invoke()
+def chat(user_input: str, history):
+    result = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+    return result["messages"][-1].content
 
 
 gr.ChatInterface(
