fa31f9f83ab3cb597fd223e6237f55a4d08bfb5b
gprasad0 <guru96392@gmail.com>
2026-04-24 10:17:06 +0530

    adding playwright implementation

--- agenticAiLab/3-langGraphImplementation/day5-implementationOfPlaywrightTool.py
+++ agenticAiLab/3-langGraphImplementation/day5-implementationOfPlaywrightTool.py
@@ -6,20 +6,17 @@ from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
 from langchain_community.tools.playwright.utils import create_async_playwright_browser
 from pprint import pprint
 import asyncio
+from playwright.async_api import async_playwright
 
 
 async def main():
-    async_browser = await create_async_playwright_browser(headless=False)
-    toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
-    tools = toolkit.get_tools()
-    pprint(tools)
-    for tool in tools:
-        pprint(f"{tool.name}={tool}")
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
 
-    tool_dict = {tool.name: tool for tool in tools}
-    pprint(tool_dict)
-    navigate_tool = tool_dict.get("navigate_browser")
-    extract_text_tool = tool_dict.get("extract_text")
+        await page.goto("https://www.cnn.com", wait_until="domcontentloaded")
+        text = await page.inner_text("body")
+        print(text)
 
 
 if __name__ == "__main__":
